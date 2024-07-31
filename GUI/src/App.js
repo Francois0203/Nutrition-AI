2ba77f896eed84a22853f23d09745df5667ea18a
Francois <francoismeiring0203@gmail.com>
2024-07-31 14:12:52 +0200

    Move GUI Folder
    Moved GUI folder to same directory as python scripts

--- GUI/src/App.js
+++ GUI/src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+      <h1 className="animated-title">Nutrition AI</h1>
       <div className="switch-container">
         <label className="switch">
           <input type="checkbox" onChange={toggleTheme} checked={darkMode} />
