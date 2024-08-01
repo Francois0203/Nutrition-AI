ab9fb42e3ec72536cf6e3d905029667709422566
Francois <francoismeiring0203@gmail.com>
2024-08-01 19:22:08 +0200

    GUI Components
    All basic GUI components are done including the output window. Spacing is still not correct.

--- nutrition-frontend/src/App.js
+++ nutrition-frontend/src/App.js
@@ -27,187 +27,222 @@ function App() {
         </label>
         <span className="mode-label">{darkMode ? 'Dark Mode' : 'Light Mode'}</span>
       </div>
-      <div className="input-container">
-        <label htmlFor="email" className="email-label">Email:</label>
-        <input 
-          id="email" 
-          type="text" 
-          className={`email-input ${darkMode ? 'dark-input' : 'light-input'}`}
-          value={email} 
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email"
-        />
-        <label htmlFor="weight" className="weight-label">Weight (kg):</label>
-        <select 
-          id="weight" 
-          className="weight-combobox"
-          value={weight} 
-          onChange={(e) => setWeight(Number(e.target.value))}
-        >
-          {Array.from({ length: 150 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label htmlFor="height" className="height-label">Height (cm):</label>
-        <select 
-          id="height" 
-          className="height-combobox"
-          value={height} 
-          onChange={(e) => setHeight(Number(e.target.value))}
-        >
-          {Array.from({ length: 250 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label htmlFor="hip" className="hip-label">Hip Circumference (cm):</label>
-        <select 
-          id="hip" 
-          className="hip-combobox"
-          value={hip} 
-          onChange={(e) => setHip(Number(e.target.value))}
-        >
-          {Array.from({ length: 200 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label htmlFor="waist" className="waist-label">Waist Circumference (cm):</label>
-        <select 
-          id="waist" 
-          className="waist-combobox"
-          value={waist} 
-          onChange={(e) => setWaist(Number(e.target.value))}
-        >
-          {Array.from({ length: 150 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label htmlFor="age" className="age-label">Age:</label>
-        <select 
-          id="age" 
-          className="age-combobox"
-          value={age} 
-          onChange={(e) => setAge(Number(e.target.value))}
-        >
-          {Array.from({ length: 100 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label htmlFor="exerciseDays" className="exercise-label">Exercise Days per Week:</label>
-        <select 
-          id="exerciseDays" 
-          className="exercise-combobox"
-          value={exerciseDays} 
-          onChange={(e) => setExerciseDays(Number(e.target.value))}
-        >
-          {Array.from({ length: 7 }, (_, i) => i + 1).map(num => (
-            <option key={num} value={num}>{num}</option>
-          ))}
-        </select>
-        <label className="option-label">What is your goal?</label>
-        <div className="radio-container">
-          <label className="radio-label">
+      <div className="container"> {/* Added container class */}
+        <div className="input-container">
+          {/* Input fields and radio buttons */}
+          <div className="row">
+            <label htmlFor="email" className="email-label">Email:</label>
             <input 
-              type="radio" 
-              name="options" 
-              value="lose_weight" 
-              checked={selectedGoal === 'lose_weight'} 
-              onChange={(e) => setSelectedGoal(e.target.value)}
+              id="email" 
+              type="text" 
+              className={`email-input ${darkMode ? 'dark-input' : 'light-input'}`}
+              value={email} 
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder="Enter your email"
             />
-            <span>Lose Weight</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="gain_weight" 
-              checked={selectedGoal === 'gain_weight'} 
-              onChange={(e) => setSelectedGoal(e.target.value)}
-            />
-            <span>Gain Weight</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="gain_lean_muscle" 
-              checked={selectedGoal === 'gain_lean_muscle'} 
-              onChange={(e) => setSelectedGoal(e.target.value)}
-            />
-            <span>Gain Lean Muscle</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="maintain_weight" 
-              checked={selectedGoal === 'maintain_weight'} 
-              onChange={(e) => setSelectedGoal(e.target.value)}
-            />
-            <span>Maintain Weight</span>
-          </label>
+          </div>
+          <div className="row">
+            <label htmlFor="age" className="age-label">Age:</label>
+            <select 
+              id="age" 
+              className="age-combobox"
+              value={age} 
+              onChange={(e) => setAge(Number(e.target.value))}
+            >
+              {Array.from({ length: 100 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+            <label htmlFor="exerciseDays" className="exercise-label">Exercise Days per Week:</label>
+            <select 
+              id="exerciseDays" 
+              className="exercise-combobox"
+              value={exerciseDays} 
+              onChange={(e) => setExerciseDays(Number(e.target.value))}
+            >
+              {Array.from({ length: 7 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+          </div>
+          <div className="row">
+            <label htmlFor="weight" className="weight-label">Weight (kg):</label>
+            <select 
+              id="weight" 
+              className="weight-combobox"
+              value={weight} 
+              onChange={(e) => setWeight(Number(e.target.value))}
+            >
+              {Array.from({ length: 150 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+            <label htmlFor="height" className="height-label">Height (cm):</label>
+            <select 
+              id="height" 
+              className="height-combobox"
+              value={height} 
+              onChange={(e) => setHeight(Number(e.target.value))}
+            >
+              {Array.from({ length: 250 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+          </div>
+          <div className="row">
+            <label htmlFor="hip" className="hip-label">Hip Circumference (cm):</label>
+            <select 
+              id="hip" 
+              className="hip-combobox"
+              value={hip} 
+              onChange={(e) => setHip(Number(e.target.value))}
+            >
+              {Array.from({ length: 200 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+            <label htmlFor="waist" className="waist-label">Waist Circumference (cm):</label>
+            <select 
+              id="waist" 
+              className="waist-combobox"
+              value={waist} 
+              onChange={(e) => setWaist(Number(e.target.value))}
+            >
+              {Array.from({ length: 150 }, (_, i) => i + 1).map(num => (
+                <option key={num} value={num}>{num}</option>
+              ))}
+            </select>
+          </div>
+          <div className="row">
+            <div className="radio-group">
+              <div className="radio-container">
+                <div className="radio-label-container">
+                  <label className="option-label">What is your goal?</label>
+                  <div className="radio-options">
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="goal" 
+                        value="lose_weight" 
+                        checked={selectedGoal === 'lose_weight'} 
+                        onChange={(e) => setSelectedGoal(e.target.value)}
+                      />
+                      <span>Lose Weight</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="goal" 
+                        value="gain_weight" 
+                        checked={selectedGoal === 'gain_weight'} 
+                        onChange={(e) => setSelectedGoal(e.target.value)}
+                      />
+                      <span>Gain Weight</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="goal" 
+                        value="gain_lean_muscle" 
+                        checked={selectedGoal === 'gain_lean_muscle'} 
+                        onChange={(e) => setSelectedGoal(e.target.value)}
+                      />
+                      <span>Gain Lean Muscle</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="goal" 
+                        value="maintain_weight" 
+                        checked={selectedGoal === 'maintain_weight'} 
+                        onChange={(e) => setSelectedGoal(e.target.value)}
+                      />
+                      <span>Maintain Weight</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="radio-container">
+                <div className="radio-label-container">
+                  <label className="option-label">What is your preferred diet type?</label>
+                  <div className="radio-options">
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="paleo" 
+                        checked={selectedDiet === 'paleo'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Paleo</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="vegan" 
+                        checked={selectedDiet === 'vegan'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Vegan</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="keto" 
+                        checked={selectedDiet === 'keto'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Keto</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="mediterranean" 
+                        checked={selectedDiet === 'mediterranean'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Mediterranean</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="dash" 
+                        checked={selectedDiet === 'dash'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Dash</span>
+                    </label>
+                    <label className="radio-label">
+                      <input 
+                        type="radio" 
+                        name="diet" 
+                        value="any" 
+                        checked={selectedDiet === 'any'} 
+                        onChange={(e) => setSelectedDiet(e.target.value)}
+                      />
+                      <span>Any</span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <label className="option-label">What is your preferred diet type?</label>
-        <div className="radio-container">
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="paleo" 
-              checked={selectedDiet === 'paleo'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Paleo</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="vegan" 
-              checked={selectedDiet === 'vegan'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Vegan</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="keto" 
-              checked={selectedDiet === 'keto'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Keto</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="mediterranean" 
-              checked={selectedDiet === 'mediterranean'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Mediterranean</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="dash" 
-              checked={selectedDiet === 'dash'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Dash</span>
-          </label>
-          <label className="radio-label">
-            <input 
-              type="radio" 
-              name="options" 
-              value="any" 
-              checked={selectedDiet === 'any'} 
-              onChange={(e) => setSelectedDiet(e.target.value)}
-            />
-            <span>Any</span>
-          </label>
+        <div className="output-container">
+          <h2>Output</h2>
+          <p><strong>Email:</strong> {email}</p>
+          <p><strong>Age:</strong> {age}</p>
+          <p><strong>Exercise Days per Week:</strong> {exerciseDays}</p>
+          <p><strong>Weight (kg):</strong> {weight}</p>
+          <p><strong>Height (cm):</strong> {height}</p>
+          <p><strong>Hip Circumference (cm):</strong> {hip}</p>
+          <p><strong>Waist Circumference (cm):</strong> {waist}</p>
+          <p><strong>Goal:</strong> {selectedGoal.replace(/_/g, ' ')}</p>
+          <p><strong>Diet Type:</strong> {selectedDiet.replace(/_/g, ' ')}</p>
         </div>
       </div>
     </div>
